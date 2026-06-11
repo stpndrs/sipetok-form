04bd39c8be4ca4e8af902d32bffdafe4ceb0b1b5
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2026-06-11 23:30:06 +0700

    feat: tenant module service

---
+++ Dto/request/TenantSaveRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sipetok_form.Models.dto.request
+{
+    internal class TenantSaveRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public UserSaveRequest User { get; set; }
+    }
+}
