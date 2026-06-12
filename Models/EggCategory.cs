125479f88f156495cc675929874b6c2e2239acb3
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2026-06-12 08:29:28 +0700

    feat: egg category service

---
+++ Models/EggCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace sipetok_form.Models
+{
+    internal class EggCategory
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int TenantId { get; set; }
+    }
+}
