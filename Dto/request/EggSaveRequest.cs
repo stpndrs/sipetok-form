135f51c3c39825562ba70f1f636f2e761f9deecb
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2026-06-12 08:33:10 +0700

    feat: egg service

---
+++ Dto/request/EggSaveRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace sipetok_form.Models.dto.request
+{
+    internal class EggSaveRequest
+    {
+        public DateTime ProductionDate { get; set; }
+        public double Stock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
